8faae714be9f498b36d0d2e7fd5db65388ef8694
oddesign <42724589+dovenokan@users.noreply.github.com>
2021-07-10 19:30:14 +0300

    fwwf

---
+++ src/App.svelte
@@ -0,0 +1,49 @@
+<script>
+	export let name;
+	window.onload = function() {
+		new Audio('https://res.cloudinary.com/odcloud/video/upload/v1625930644/notal.mp3_ycwskc.mp3').play()
+	}
+
+</script>
+
+<main>
+	<nav>
+		<li>La</li>
+		<li>La</li>
+		<li>La</li>
+		<li>La</li>
+	</nav>
+	<textarea name="" id="" ></textarea>
+</main>
+
+
+<style>
+	main {
+		text-align: center;
+		font-size: 5vh;
+	}
+
+	li{
+		list-style: none;
+		margin: 15px;
+	}
+
+	nav{
+		background-color: rgba(6, 17, 75, 0.98);
+		height: 8vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	textarea{
+		resize: none;
+		border: transparent;
+		width: 200vh;
+		height: 90vh;
+		background-color: rgb(11 4 37);
+		color: whitesmoke;
+		font-size: 3vh;
+		font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+	}
+</style>
